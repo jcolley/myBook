d90e7a6155b84aec74fd1c8b615e0d34d0a037b3
John Colley <webjocky@gmail.com>
2017-06-28 12:37:33 -0700

    addblog page complete

--- apps/my_book/urls.py
+++ apps/my_book/urls.py
@@ -3,5 +3,6 @@ from.import views
 
 app_name = 'mybook'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^addBlog$', views.addBlog, name='addBlog'),
 ]
--- apps/my_book/views.py
+++ apps/my_book/views.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from ..login.models import User
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'my_book/index.html')
+    user = User.objects.get(id=request.session.get('id'))
+    context = {
+        'user': user,
+    }
+    return render(request, 'my_book/index.html', context)
+
+
+def addBlog(request):
+    return render(request, 'my_book/addBlog.html')
+
